88ce151f15520a0d2651817c74c7411063b663cd
guozq0611 <guozq0611@gmail.com>
2025-01-24 12:38:47 +0800

    some modify, nothing import @office

---
+++ btc_model/core/update_data/update_by_okx.py
@@ -0,0 +1,19 @@
+# -*- coding: UTF-8 –*-
+
+"""
+author:
+
+create date: 2025-01-24
+
+content:
+
+"""
+
+import numpy as np
+import pandas as pd
+import datetime
+
+class OKxDownloader:
+    def __init__(self):
+        pass
+
